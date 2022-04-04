e86258675660eca85a396bb85dfabd1c08d358ba
HyunsuChoi-git <60773039+HyunsuChoi-git@users.noreply.github.com>
2022-04-04 21:34:01 +0900

    study_aop

--- spring_first/src/main/java/com/herachoicomp/spring/aspect/SamleAspect.java
+++ spring_first/src/main/java/com/herachoicomp/spring/aspect/SamleAspect.java
@@ -1,17 +1,18 @@
 package com.herachoicomp.spring.aspect;
 
-
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
+@EnableAspectJAutoProxy
 @Component
-@Aspect   //어드바이스(타겟에 적용되는 코드 자체)+포인트 컷(적용할 타겟. 어떤 메소드.)
-public class SamleAspect {
-	
+@Aspect 
+//어드바이스(타겟에 적용되는 코드 자체)+포인트 컷(적용할 타겟. 어떤 메소드.)
+public class SamleAspect {	
 	//before어드바이스. Joinpoint를 파라미터로 받을 수 있다. (타겟정보)
-	// "execution(* com.herachoicomp.spring..*.*(..))" --> 포인트컷 문법. 
+	// "execution(* com.herachoicomp.spring..*.*(..))" --> 포인트컷 문법.
 	// 괄호안->실제 메소드. 해당 루트안의 모든 메소드
 	@Before("execution(* com.herachoicomp.spring..*.*(..))")
 	public void before(JoinPoint joinPoint) {
